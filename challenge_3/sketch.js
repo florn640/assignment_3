6a50e4b5cfe56e2d3c48fb04aaf57aaaa4b4275b
nat1508 <florn640@newschool.edu>
2015-09-23 15:44:34 -0400

    challenge3
    almost done

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -1,18 +1,21 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
-	noStroke();
-	noLoop();
+	createCanvas(700, 500);
+	noFill();
 }
-
 function draw() {
-	// clear the background
-	background(150, 50, 150);
+	//noStroke();
+ if (mouseIsPressed) {
+	 background(46, 42, 105)
+} else {
+	 background(153, 13, 13)
+ }
+//draw circle
+stroke(255, 255, 255);
+ellipse(mouseX, mouseY, 40, 40);
+line(mouseX, mouseY, 0, mouseY);
+line(mouseX, mouseY, mouseX, 0);
+line(700, mouseY, mouseX, mouseY);
+line(mouseX,700, mouseX, mouseY);
 
-	// set a fill color
-	fill(255, 255, 255);
-
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
 }
