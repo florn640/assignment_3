ffb6a5e6da62954b3d398b8a4e8855e270f6ff29
nat1508 <florn640@newschool.edu>
2015-09-24 23:11:33 -0400

    bonus challenge

---
+++ challenge_BONUS/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+	createCanvas(600, 500);
+
+
+}
+
+function draw() {
+	background(0, 0, 0);
+	noStroke();
+	fill(255, 255, 255);
+
+
+	for (var i = 0; i < 1; i++){
+		ellipse(mouseX+random(-5, 5), mouseY+random(-5, 5), 50, 50);
+		
+	}
+}
