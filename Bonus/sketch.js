32f03ace09c605a33d691cc9c38f9c78e998ca67
nat1508 <florn640@newschool.edu>
2015-09-25 15:57:39 -0400

    challenges

--- Bonus/sketch.js
+++ Bonus/sketch.js
@@ -1,17 +1,38 @@
 function setup() {
-	createCanvas(600, 500);
-
-
+	// create canvas
+	createCanvas(700, 500);
+	background (0);
+	fill(152, 13, 13);
+frameRate(100);
 }
 
 function draw() {
-	background(0, 0, 0);
-	noStroke();
-	fill(255, 255, 255);
+//draw red squares
 
+//rect(mouseX, mouseY, 40, 40);
 
-	for (var i = 0; i < 1; i++){
-		ellipse(mouseX+random(-5, 5), mouseY+random(-5, 5), 50, 50);
-		
+//rect(mouseX -21, mouseY, 40, 40);
+//rect(mouseX, mouseY -21, 40, 40);
+//rect(700, mouseY, mouseX +21, mouseY);
+//rect(mouseX,700, mouseX, mouseY +21);
+var cuBes= random(0,1);
+console.log("The random cuBes value is: " + cuBes);
+// draw UFO
+if (cuBes > 0 && cuBes < 0.20) {
+fill (0, 0, 0)
+rect(10, 0, 55, 55);
+} else if (cuBes > 0.21 && cuBes < 0.45) {
+fill (255, 255, 255)
+rect(0, 20, 55, 55);
+} else if (cuBes > 0.46 && cuBes < 0.60) {
+fill (191, 191, 191)
+rect(100, 0, 55, 55);
+} else if (cuBes > 0.61 && cuBes < 0.85) {
+fill(140, 140, 140)
+rect(0, 300, 55, 55);
+} else {
+fill(100, 100, 100)
+rect(0, 100, 55, 55);
+
 	}
 }
--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -9,9 +9,11 @@ function draw() {
 	noStroke();
 	fill(255, 255, 255);
 
+//function mouseMoved() {
 
 	for (var i = 0; i < 1; i++){
 		ellipse(mouseX+random(-5, 5), mouseY+random(-5, 5), 50, 50);
-		
+
 	}
+
 }
